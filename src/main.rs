516943814c5a690c5a6c82e053e51e98604f7f5b
Jimmy Chen <jchen.tc@gmail.com>
2023-08-01 16:01:24 +1000

    add rust 1.69

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use tokio::runtime::Builder;
-
 fn main() {}
 
 #[test]
+#[ignore]
 fn test_boxed() {
-    let a = Box::new([-1; 3000000]);
+    let _a = Box::new([-1; 3000000]);
+}
+
+#[test]
+fn test_boxed_over_2mb() {
+    // passes on linux and macos, fails on windows
+    let _a = Box::new([0u8; 2 * 1024 * 1024 - 1024 * 9]);
+
+    // passes on macos, fails on linux and windows
+    // let _a = Box::new([0u8; 2 * 1024 * 1024 + 1024 * 8]);
 }
